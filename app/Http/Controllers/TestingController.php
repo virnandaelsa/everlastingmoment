ae109e21662f1f0f6fdb3a80e1cd3a2ba7d511bc
alfianfic <alfiac999@gmail.com>
2024-06-22 11:58:51 +0700

    testing

--- app/Http/Controllers/TestingController.php
+++ app/Http/Controllers/TestingController.php
@@ -3,12 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\detailPJ;
+use App\Models\katalog;
+use App\Models\dt_katalog;
+use App\Models\transaksi;
+use App\Models\dt_transaksi;
+use App\Models\review;
 
 class TestingController extends Controller
 {
     public function t1()
     {
-        return view('testing.t1');
+        $user = User::with('detailPJ');
+        return view('testing.t1', compact('user'));
     }
     public function t2()
     {
--- resources/views/testing/t1.blade.php
+++ resources/views/testing/t1.blade.php
@@ -1,1 +1,5 @@
 t1
+@foreach ($user as $data )
+    {{ $data->nama }}
+    <br>
+@endforeach
