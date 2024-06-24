254c82d684992195b15094f988c182f3014d95d9
alfianfic <alfiac999@gmail.com>
2024-06-24 11:33:45 +0700

    edit sidebar dan beranda

---
+++ resources/views/customer/beranda/pjBeranda.blade.php
@@ -0,0 +1,61 @@
+    <div class="penawaran my-4">
+        @auth @if (auth()->user()->role==1)
+        <h4 class="bold-text">Tambah Katalog Anda Sekarang!</h4>
+        <button>TAMBAH KATALOG</button>            
+        @endif @endauth
+    </div>
+    
+    @auth @if (auth()->user()->role==1)
+    <div class="recommendations my-4">
+        <h4 class="bold-text">KATALOG SAYA</h4>
+        <div class="d-flex flex-wrap">
+            <div class="card">
+                <a href="{{ asset('images/mua.jpg') }}">
+                    <img src="{{ asset('images/mua.jpg') }}" class="card-img-top" alt="MUA">
+                </a>
+                    {{-- <div class="card-body">
+                    <h5 class="card-title">Wedding Make Up Paket</h5>
+                    <p class="card-text">Rp. 5.000.000,00</p>
+                    <div class="move-right">
+                        <a href="/lihatjasa" class="">Lihat detail</a>
+                    </div>
+                </div> --}}
+            </div>
+            <div class="card">
+                <a href="{{ asset('images/mua.jpg') }}">
+                    <img src="{{ asset('images/mua.jpg') }}" class="card-img-top" alt="MUA">
+                </a>
+            </div>
+            <div class="card">
+                <a href="{{ asset('images/mua.jpg') }}">
+                    <img src="{{ asset('images/mua.jpg') }}" class="card-img-top" alt="MUA">
+                </a>
+            </div>
+            <div class="card">
+                <a href="{{ asset('images/mua.jpg') }}">
+                    <img src="{{ asset('images/mua.jpg') }}" class="card-img-top" alt="MUA">
+                </a>
+            </div>
+            <div class="card">
+                <a href="{{ asset('images/mua.jpg') }}">
+                    <img src="{{ asset('images/mua.jpg') }}" class="card-img-top" alt="MUA">
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="recommendations my-4">
+        <h4 class="bold-text">REVIEW</h4>
+        <div class="d-flex flex-wrap">
+            <div class="card">
+                <img src="{{ asset('images/mua.jpg') }}" class="card-img-top" alt="MUA">
+                <div class="card-body">
+                    <h5 class="card-title">Wedding Make Up Paket</h5>
+                    <p class="card-text">Rp. 5.000.000,00</p>
+                    <div class="move-right">
+                        <a href="/lihatjasa" class="">Lihat detail</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endif @endauth
